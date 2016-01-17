6ff40173851c6fe6a1fe46f15c427853c6521d8b
Denis Anchugov <denis.anchugov@outlook.com>
2016-01-17 10:41:35 +0200

    Implemented ExpressionController, expressionFactory. Removed demo project files.

--- TrainYourBrain/www/js/controllers.js
+++ TrainYourBrain/www/js/controllers.js
@@ -1,20 +1,72 @@
-angular.module('starter.controllers', [])
+angular
+    .module('trainBrain.controllers', [])
+    .controller('ExpressionController', ExpressionController);
 
-.controller('DashCtrl', function($scope) {})
+function ExpressionController($interval, expressionService) {
+    var timer;
+    var vm = this;
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+    vm.deployExpression = function () {
+        if (vm.lives > 0) {
+            vm.currentExpression = expressionService.getExpression();
+        } else {
+            
+        }
+    };
 
-.controller('ChatDetailCtrl', function ($scope, $stateParams, ExpressionService) {
-    console.log(ExpressionService.getExpression());
-})
+    vm.eraseChar = function () {
+        if (vm.userInput.length != 0) {
+            vm.userInput = vm.userInput.slice(0, -1);
+        }
+    };
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    vm.appendChar = function (char) {
+        vm.userInput = vm.userInput + char;
+    };
+
+    vm.checkAnswer = function () {
+        var isCorrect = vm.currentExpression.checkAnswer(parseInt(vm.userInput));
+
+        if (isCorrect) {
+            vm.score++;
+        } else {
+            vm.lives--;
+        }
+
+        vm.timer = 5;
+        vm.userInput = '';
+        vm.deployExpression();
+    };
+
+    vm.start = function myfunction() {
+        init(vm);
+        if (timer) {
+            $interval.cancel(timer);
+        }
+        timer = $interval(timerTick, 1000)
+    };
+
+    var timerTick = function () {
+        vm.timer--;
+        if (vm.timer == 0) {
+            vm.lives--
+            if (vm.lives == 0) {
+                $interval.cancel(timer);
+            } else {
+                vm.deployExpression();
+                vm.timer = 5;
+            }
+        }
+    };
+}
+
+
+
+var init = function (scope) {
+    scope.timer = 5;
+    scope.score = 0;
+    scope.lives = 3;
+    scope.userInput = '';
+
+    scope.deployExpression();
+};
