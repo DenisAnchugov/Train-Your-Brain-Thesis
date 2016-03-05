0cea8105ad3bf4d09ff2c79f9a80b374d0f38793
Denis Anchugov <denis.anchugov@futurice.com>
2016-03-05 21:36:31 +0200

    Added Xamarin projects.

---
+++ TrainYourBrain.Xamarin.Windows/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TrainYourBrain.Xamarin.Windows
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new TrainYourBrain.Core.App());
+        }
+    }
+}
